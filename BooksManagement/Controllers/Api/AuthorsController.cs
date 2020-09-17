6b4c9c383d581743d6fc913fc89bda771a03449f
Sergio Pastor Ontiveros Pérez <sergio.ontiveros.perez@outlook.com>
2020-09-17 17:10:31 -0300

    Add project files.

---
+++ BooksManagement/Controllers/Api/AuthorsController.cs
@@ -0,0 +1,52 @@
+﻿using BooksManagement.Context;
+using BooksManagement.Entities;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BooksManagement.Controllers.Api
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthorsController : ControllerBase
+    {
+        private readonly ApplicationDbContext _context;
+
+        public AuthorsController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public ActionResult<IEnumerable<Author>> Get()
+        {
+            return _context.Authors.ToList();
+        }
+
+        [HttpGet("{id}", Name = "GetAuthor")]
+        public ActionResult<Author> Get(int id)
+        {
+            var author = _context.Authors.FirstOrDefault(a => a.Id == id);
+
+            if (author == null)
+                return NotFound();
+
+            return author;
+        }
+
+        [HttpPost]
+        public ActionResult Post([FromBody] Author author)
+        {
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState);
+
+            _context.Authors.Add(author);
+            _context.SaveChanges();
+
+            return new CreatedAtRouteResult("GetAuthor", new { id = author.Id }, author);
+        }
+    }
+}
---
+++ BooksManagement/Entities/Author.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BooksManagement.Entities
+{
+    public class Author
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(255)]
+        public string Name { get; set; }
+        
+        [Required]
+        [StringLength(255)]
+        public string Email { get; set; }
+    }
+}
